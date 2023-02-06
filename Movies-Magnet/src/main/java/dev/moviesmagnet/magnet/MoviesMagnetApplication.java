d862fafab3f3107f2d36eba2129602775b7ac5e3
Essam Mohamed <50668640+EssamMohamedAbo-ElMkarem@users.noreply.github.com>
2023-02-06 14:06:48 +0200

    Backend part written using Java spring boot

---
+++ Movies-Magnet/src/main/java/dev/moviesmagnet/magnet/MoviesMagnetApplication.java
@@ -0,0 +1,15 @@
+package dev.moviesmagnet.magnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MoviesMagnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesMagnetApplication.class, args);
+	}
+
+}
---
+++ Movies-Magnet/src/test/java/dev/moviesmagnet/magnet/MoviesMagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.moviesmagnet.magnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesMagnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
